9d8f15a795170f80c94d0467d8d8ed171a078d63
Irene Sun <93227340+IreneRSun@users.noreply.github.com>
2022-11-23 13:39:14 -0700

    updated for working with new view

--- venue_search.py
+++ venue_search.py
@@ -1,62 +1,21 @@
 class TopVenues:
-    def __init__(self, collection, referencing_view):
-        self.col = collection
-        self.rcol = referencing_view
-        self.venues = dict()
-        self.add_venue_info()
+    def __init__(self, venue_view):
+        self.view = venue_view
+        self.venues = self.load_venues()
 
-    def add_venue_info(self):
-        # get venue and article count for venue
-        venue_articles = self.col.aggregate([
-            {
-                "$match": {
-                    "venue": {"$ne": ""}
-                }
-            },
-            {"$group":
-                {
-                    "_id": {"venue": "$venue"},
-                    "num_articles": {"$sum": 1}
-                }
-            }
+    def load_venues(self):
+        # get sorted list of venue info
+        ordered_venues = self.view.aggregate([
+            {"$sort": {"venue_references": -1}}
         ])
-        # add them to the self.venues dictionary
-        for va in venue_articles:
-            venue = va["_id"]["venue"]
-            num_articles = va["num_articles"]
-            self.venues[venue] = [num_articles]
-
-    def get_top_venues(self, n):
-        #---------implement---------
-        # find top n venues by getting the number of articles referencing each venue
-        # and sorting accordingly
-        # then add info to self.venues
-        # and return a list of the top n venues in order
-        venue_references = self.rcol.aggregate([
-            {
-                "$group": {
-                    "_id": {"venue": "$venue"},
-                    "num_referencing": {"$sum": {"$size": "$referencing"}}
-                }
-            },
-            {"$sort": {"num_referencing": -1}},
-            {"$limit": n}
-        ])
-        venues = []
-        for vr in venue_references:
-            venue = vr["_id"]["venue"]
-            venues.append(venue)
-            num_referencing = vr["num_referencing"]
-            self.venues[venue].append(num_referencing)
-        return venues
+        return list(ordered_venues)
 
     def show_venues(self, n):
-        count = 1
-        venues = self.get_top_venues(n)
-        for venue in venues:
-            num_articles = self.venues[venue][0]
-            num_references = self.venues[venue][1]
-            print(f"{count} venue: {venue}, ",
+        # print the first n items of self.venues
+        for i in range(0, n):
+            venue = self.venues[i]
+            num_articles = venue["venue_count"]
+            num_references = venue["venue_references"]
+            print(f"{i+1} venue: {venue}, ",
                   f"number of articles: {num_articles}, ",
                   f"number of references to venue's articles: {num_references}")
-            count += 1
